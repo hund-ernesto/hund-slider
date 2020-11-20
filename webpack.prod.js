45649dd29a5408ea443db3da2bfa5e5b5f1e2ee8
Ernesto Bellei <ernesto@hund.studio>
2020-11-20 14:40:20 +0100

    Fixed prod wrong entry point

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = merge( common, {
   mode: 'production',
-  entry: './examples/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
